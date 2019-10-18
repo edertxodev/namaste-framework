b8de363bf6fba194fbdef0ea10597cb564ec3173
Eder <eferreira@feelfreerentals.com>
2019-10-18 16:50:32 +0200

    Animated button

--- example/src/module/index.tsx
+++ example/src/module/index.tsx
@@ -12,7 +12,8 @@ import {
     CardActions,
     Button,
     TextInput,
-    SelectInput
+    SelectInput,
+    AnimatedButton
 } from 'namaste-framework'
 
 const App = () => {
@@ -24,22 +25,22 @@ const App = () => {
     <Container>
       <Row>
         <Column desktop={6}>
-          <Card shadow bordered>
-            <CardHeader>
-              <CardTitle>Kaixo</CardTitle>
-              <CardSubtitle>Frogatxo bat da</CardSubtitle>
-            </CardHeader>
-            <CardImage src="https://psicologiaymente.com/media/Qd/Vq/Ow/QdVqOweqyD/namaste-significado/namaste-significado-social.jpg" />
-            <CardContent>
-              <p>Lorem fistrum papaar papaar benemeritaar se calle ustée ese que.</p>
-            </CardContent>
-            <CardActions>
-              <Button shadow>Example</Button>
-              <Button shadow view="primary">Example</Button>
-              <Button shadow view="secondary">Example</Button>
-              <Button view="disabled">Example</Button>
-            </CardActions>
-        </Card>
+            <Card shadow bordered>
+              <CardHeader>
+                <CardTitle>Kaixo</CardTitle>
+                <CardSubtitle>Frogatxo bat da</CardSubtitle>
+              </CardHeader>
+              <CardImage src="https://psicologiaymente.com/media/Qd/Vq/Ow/QdVqOweqyD/namaste-significado/namaste-significado-social.jpg" />
+              <CardContent>
+                <p>Lorem fistrum papaar papaar benemeritaar se calle ustée ese que.</p>
+              </CardContent>
+              <CardActions>
+                <Button shadow block>Example</Button>
+                <Button shadow view="primary">Example</Button>
+                <Button shadow view="secondary">Example</Button>
+                <Button view="disabled">Example</Button>
+              </CardActions>
+          </Card>
         </Column>
         <Column desktop={3}>
           <TextInput label="Kaixo" value="Example" />
@@ -47,6 +48,9 @@ const App = () => {
         <Column desktop={3}>
           <SelectInput options={selectOptions} />
         </Column>
+      </Row>
+      <Row>
+        <AnimatedButton>Click me!</AnimatedButton>
       </Row>
     </Container>
   )
